9eb17e4ddde8eb623f4d225e25ea98a14d5e9d64
ElMaestro157 <Dimapopov.ru@yandex.ru>
2017-05-27 05:14:03 +0300

    TableView

--- iShop/ViewController.swift
+++ iShop/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.items = loadPlist()
     }
 
     override func didReceiveMemoryWarning() {
@@ -91,5 +92,48 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBOutlet weak var table: UITableView!
+    
+    @IBOutlet weak var descriptionField: UITextView!
+    
+    var items = [[String:String]]()
+    
+    func loadPlist()->[[String:String]]
+    {
+        let path = NSBundle.mainBundle().pathForResource("iPlist", ofType: "plist")
+        return NSArray(contentsOfURL: NSURL(fileURLWithPath: path!)!) as [[String:String]]
+    }
+    
 }
+
+extension ViewController:UITableViewDataSource,UITableViewDelegate
+{
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("iCell") as UITableViewCell
+        
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item["title"]
+        cell.detailTextLabel?.text = item["description"]
+        cell.imageView?.image = UIImage(named: item["image"]!)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 120
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let item = items[indexPath.row]
+        descriptionField.text = item["description"]
+
+    }
+    
+    
+}
+
 
